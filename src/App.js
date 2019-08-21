f753261f08e286db4d4ae67117c3efd321858ce8
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-21 15:07:17 -0700

    setStatus sort of working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
 
+
+
+const handleUsers = user =>{
+  
+console.log(user);
+}
+
 function App() {
+  const [user, setUser] = useState([]);
+
+  
   return (
     <div className="App">
-      <Form />
+      <Form handleUsers={handleUsers}/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
+// const handleUser = (name,email) =>{
+//     console.log(name);
+//     console.log(email);
+//     setUser(name)
+// }
+// { values, errors, touched },...props
+const LoginForm = (props) => {
+    let values=props.values;
+    let errors = props.errors;
+    let touched = props.touched;
+    const [user, setUser] = useState([]); 
+    // console.log(props.status)
 
-
-const LoginForm = ({ values, errors, touched }) => {
-    const [user, setUser] = useState(); 
+    // if(props.status != ''){
+    //     setUser(props.status)
+    //     props.setStatus('');
+    // }
   
   return (
 
@@ -56,22 +69,28 @@ const FormikLoginForm = withFormik({
   }),
   //======END VALIDATION SCHEMA==========
 
-  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
-
+  handleSubmit(values, { resetForm, setErrors, setSubmitting, setStatus }) {
+    if (values.email === "waffle@syrup.com") {
+        setErrors({ email: "That email is already taken" });
+      } else {
       axios
         .post("https://reqres.in/api/users", values)
         .then(res => {
-          console.log(res); // Data was created successfully and logs to console
+        //   console.log(res); // Data was created successfully and logs to console
           resetForm();
           setSubmitting(false);
           let user = res.data.name;
           let email = res.data.email;
-          setUser([...user, {name:user, email:email}])
+        //   setStatus({ name: user, email:email });
+        //   setUser([...user, {name:user, email:email}])
+        // handleUser(user, email)
+        //   console.log(props.handleUsers);
         })
         .catch(err => {
           console.log(err); // There was an error creating the data and logs to console
           setSubmitting(false);
         });
+    }
 
   }
 })(LoginForm);
