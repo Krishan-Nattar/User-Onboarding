bd26e09a1f5a62585256aa8c16f68cab984d7ed7
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-22 11:12:32 -0700

    Refactor. MVP + Stretch done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Form from './components/Form';
 
 
-
-// const handleUsers = user =>{
-  
-// console.log(user);
-// }
-
 function App() {
-  // const [user, setUser] = useState([]);
-
 
   return (
     <div className="App">
